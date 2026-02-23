d49540ca635f10f089ce5e7281a2b54ec60b4f1d
Rajat <rajat804@gmail.com>
2026-02-23 17:26:14 +0530

    cart js model

---
+++ models/Cart.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, default: 1 },
+        size: String,
+        color: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", cartSchema);
