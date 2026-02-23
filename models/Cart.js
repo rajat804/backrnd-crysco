c2187b253b5a80f5f6dbe516ec2376f37b7b8f5a
Rajat <rajat804@gmail.com>
2026-02-23 17:32:27 +0530

    cart

--- models/Cart.js
+++ models/Cart.js
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 const cartSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    products: [
-      {
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-        quantity: { type: Number, default: 1 },
-        size: String,
-        color: String,
-      },
-    ],
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, default: 1 },
+    size: { type: String, default: null },
+    color: { type: String, default: null },
+    image: { type: String },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("Cart", cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+export default Cart;
