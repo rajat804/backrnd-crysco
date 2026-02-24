fa1262b180eb54de3d3594366f767beeed0429cd
Rajat <rajat804@gmail.com>
2026-02-24 14:39:42 +0530

    cart page

--- models/Cart.js
+++ models/Cart.js
@@ -1,18 +1,33 @@
 import mongoose from "mongoose";
 
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  title: String,
+  price: Number,
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+  size: String,
+  color: String,
+  image: String,
+});
+
 const cartSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, default: 1 },
-    size: { type: String, default: null },
-    color: { type: String, default: null },
-    image: { type: String },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true, // ek user ka ek cart
+    },
+    items: [cartItemSchema],
   },
   { timestamps: true }
 );
 
-const Cart = mongoose.model("Cart", cartSchema);
-export default Cart;
+export default mongoose.model("Cart", cartSchema);
