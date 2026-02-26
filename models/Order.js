e3905ae12097f0e8e6e2666b420ac71e90d1dd9c
Rajat <rajat804@gmail.com>
2026-02-26 16:01:26 +0530

    order model update

--- models/Order.js
+++ models/Order.js
@@ -2,9 +2,25 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    items: Array,
-    shippingAddress: Object,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        productId: String,
+        title: String,
+        price: Number,
+        quantity: Number,
+        size: String,
+        color: String,
+        image: String,
+      },
+    ],
+    shippingAddress: {
+      fullName: String,
+      phone: String,
+      address: String,
+      city: String,
+      pincode: String,
+    },
     totalAmount: Number,
     razorpayOrderId: String,
     razorpayPaymentId: String,
