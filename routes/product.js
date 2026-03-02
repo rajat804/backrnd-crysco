95a1665cfaa82f22fbd990f23e38b1a26dcda42a
Rajat <rajat804@gmail.com>
2026-03-02 18:24:29 +0530

    product routes

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,25 @@
 import express from "express";
 import multer from "multer";
-import { addProduct, getProducts, getProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
+import { addProduct, getProducts, getProduct, updateProduct, deleteProduct} from "../controllers/productController.js";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 // CRUD
+// CRUD routes
 router.post("/", upload.array("images", 10), addProduct);
+
+// GET all OR filtered products by category
 router.get("/", getProducts);
+
+// GET single product by ID
 router.get("/:id", getProduct);
-router.put("/:id", upload.single("image",10), updateProduct);
+
+// Update product
+router.put("/:id", upload.single("image"), updateProduct);
+
+// Delete product
 router.delete("/:id", deleteProduct);
 
 export default router;
